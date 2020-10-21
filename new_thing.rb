1b88677a356a5ea73b6029f6553a12e375265018
Anthony Alcorn <xmastony@gmail.com>
2020-10-21 15:55:02 +0000

    Automatically backed up by Learn

---
+++ new_thing.rb
@@ -0,0 +1,22 @@
+the_beatles = ["john", "george", "paul", "ringo"]
+
+p the_beatles
+
+english_bands_by_city = {
+ :liverpool =>   "The Beatles",
+ :manchester =>  "The Smiths",
+ :coventry =>    "Delia Derbyshire and the BBC Radiophonic Band",
+ :london =>      "Ziggy Stardust and the Spiders from Mars"
+}
+
+p english_bands_by_city
+
+spice_rack = [
+  ["Mace", "Ginger", "Marojam"],          
+  ["Paprika", "Fajita Mix", "Coriander"], 
+  ["Parsley", "Sage", "Rosemary"]         
+]
+
+spice_rack[1][1] = "Extract of Pizza"
+
+p spice_rack
